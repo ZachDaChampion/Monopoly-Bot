45d10230d3e0ffcd60695587643bc23bdacdf6e2
ZachDaChampion <zachchampion79@gmail.com>
2020-07-24 17:50:13 -0400

    Add custom avatar support

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,17 @@ import * as DiscordJS from "discord.js";
 const client = new DiscordJS.Client();
 
 // init environment variables from .env
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
 
 // login message
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  // set avatar
+  client.user
+    .setAvatar("res/avatar.png")
+    .then((user) => console.log(`Avatar set!`));
 });
 
 // log in to bot
