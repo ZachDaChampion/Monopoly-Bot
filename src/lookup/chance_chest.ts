f62990774a56657e5cac20d9372ddc7b552404a1
ZachDaChampion <zachchampion79@gmail.com>
2020-07-28 01:16:56 -0400

    Add all community chest cards (incomplete actions)

--- src/lookup/chance_chest.ts
+++ src/lookup/chance_chest.ts
@@ -3,10 +3,10 @@ import { Player } from "../types/player";
 export default {
   // chance
   "chance-0": {
-    description: "Advance to Go",
+    description: "Advance to GO.",
     action: (player: Player, board: Board, ruleset: Ruleset) => {
       player.location = "go";
-      player.cash += 200 * (ruleset.doubleOnGo ? 2 : 1);
+      // TODO: payment and stuff
     },
   } as Chance,
 
@@ -130,4 +130,121 @@ export default {
       // TODO: idk just kinda do the thing
     },
   } as Chance,
+
+  // community chest
+  "chest-0": {
+    description: "You have won second prize in a beauty contest. Collect $10.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash += 10;
+    },
+  } as CommunityChest,
+
+  "chest-1": {
+    description: "It is your birthday. Collect $10 from every player.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      // TODO: idk just kinda do the thing
+    },
+  } as CommunityChest,
+
+  "chest-2": {
+    description: "From sale of stock you get $50.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash += 50;
+    },
+  } as CommunityChest,
+
+  "chest-3": {
+    description: "Doctor's fees. Pay $50.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash -= 50;
+    },
+  } as CommunityChest,
+
+  "chest-4": {
+    description: "Income tax refund. Collect $20.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash += 20;
+    },
+  } as CommunityChest,
+
+  "chest-5": {
+    description: "Bank error in your favor. Collect $200.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash += 200;
+    },
+  } as CommunityChest,
+
+  "chest-6": {
+    description: "Pay hospital fees of $100.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash += 100;
+    },
+  } as CommunityChest,
+
+  "chest-7": {
+    description: "Life insurance matures. Collect $100.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash += 100;
+    },
+  } as CommunityChest,
+
+  "chest-8": {
+    description: "Holiday fund matures. Receive $100.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash += 100;
+    },
+  } as CommunityChest,
+
+  "chest-9": {
+    description:
+      "You are assessed for street repairs: $40 per house, $115 per hotel.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      // TODO: idk just kinda do the thing
+    },
+  } as CommunityChest,
+
+  "chest-10": {
+    description:
+      'Go to jail. Go directly to jail, do not pass "GO", do not collect $200.',
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.gotoJail();
+    },
+  } as CommunityChest,
+
+  "chest-11": {
+    description: "Receive $25 consultancy fee.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash += 25;
+    },
+  } as CommunityChest,
+
+  "chest-12": {
+    description: "You inherit $100.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash += 100;
+    },
+  } as CommunityChest,
+
+  "chest-13": {
+    description:
+      "Get out of jail free. This card may be kept until needed or traded.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.jail.getOutFreeCards += 1;
+    },
+  } as CommunityChest,
+
+  "chest-14": {
+    description: "Advance to GO.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.location = "go";
+      // TODO: payment and stuff
+    },
+  } as CommunityChest,
+
+  "chest-15": {
+    description: "Pay school fees of $50.",
+    action: (player: Player, board: Board, ruleset: Ruleset) => {
+      player.cash -= 50;
+    },
+  } as CommunityChest,
 } as Record<BoardEventId, BoardEvent>;
