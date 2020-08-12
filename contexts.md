ec471dbc675f718c29a5faab339e0cd604a2e794
ZachDaChampion <zachchampion79@gmail.com>
2020-08-12 14:18:20 -0400

    Add mortgage support to base context

--- contexts.md
+++ contexts.md
@@ -1,4 +1,5 @@
 - ~~base~~
 - ~~insufficient funds~~
-- offer
+- ~~offer~~
 - auction
+- jail
--- src/context/base-context.ts
+++ src/context/base-context.ts
@@ -1,30 +1,35 @@
 import { Game } from "../game";
 import { mention } from "../lib";
+import cardLookup from "../lookup/cards";
 
 export class ContextBase implements Context {
   constructor(game: Game) {
     this.game = game;
-    this.player = this.game.currentTurn;
+    this.playerId = this.game.currentTurn;
     this._hasRolled = false;
     this._doublesStreak = 0;
     this._exitApproved = false;
   }
 
   private game: Game;
-  private player: PlayerId;
+  private playerId: PlayerId;
 
   private _hasRolled: boolean;
   private _doublesStreak: 0 | 1 | 2 | 3;
   private _exitApproved: boolean;
 
   get canExit() {
-    return this._exitApproved;
+    return this._exitApproved || this.player.jail.inJail;
+  }
+
+  get player() {
+    return this.game.players[this.playerId];
   }
 
   setup() {}
 
   enter() {
-    this.game.sendMessage(`It is ${mention(this.player)}'s turn.`);
+    this.game.sendMessage(`It is ${mention(this.playerId)}'s turn.`);
     // this.game.queueMessage(
     //   `${mention(this.player)} is in jail and has ${
     //     3 - this.game.players[this.player].jail.rolls
@@ -33,13 +38,10 @@ export class ContextBase implements Context {
   }
 
   exit() {
-    this.game.sendMessage(`${mention(this.player)} has ended their turn.`);
+    this.game.sendMessage(`${mention(this.playerId)} has ended their turn.`);
   }
 
-  handleCommand(
-    command: Command,
-    properties: Array<{ prop: string | null; data: string }>
-  ) {
+  handleCommand(command: Command, properties: Record<string, any>) {
     switch (command) {
       case "ROLL": {
         // roll dice
@@ -48,7 +50,7 @@ export class ContextBase implements Context {
           Math.floor(Math.random() * 6 + 1),
         ];
         this.game.queueMessage(
-          `${mention(this.player)} rolled a **${dice[0]}** and a **${
+          `${mention(this.playerId)} rolled a **${dice[0]}** and a **${
             dice[1]
           }**!`
         );
@@ -62,7 +64,7 @@ export class ContextBase implements Context {
           if (dice[0] === 1 && this.game.ruleset.snakeEyesBonus > 0) {
             this.game.giveMoney(this.game.ruleset.snakeEyesBonus);
             this.game.queueMessage(
-              `Snake eyes! ${mention(this.player)} gets a bonus of $${
+              `Snake eyes! ${mention(this.playerId)} gets a bonus of $${
                 this.game.ruleset.snakeEyesBonus
               }!`
             );
@@ -72,7 +74,7 @@ export class ContextBase implements Context {
           if (this._doublesStreak >= 3) {
             this.game.queueMessage(
               `Tough luck! ${mention(
-                this.player
+                this.playerId
               )} rolled three doubles in a row and must now go to jail.`
             );
             this._doublesStreak = 0;
@@ -85,8 +87,8 @@ export class ContextBase implements Context {
         this.game.queueMessage(`Move ${dice[0] + dice[1]} spaces.`);
         this.game.movePlayerBy(dice[0] + dice[1]);
         this.game.queueMessage(
-          `${mention(this.player)} landed on ${
-            this.game.tileOf(this.player).name
+          `${mention(this.playerId)} landed on ${
+            this.game.tileOf(this.playerId).name
           }.`
         );
         // TODO: rent
@@ -99,7 +101,29 @@ export class ContextBase implements Context {
       }
 
       case "MORTGAGE": {
-        // TODO: this
+        switch (this.game.mortgate(properties.prop, this.playerId)) {
+          case "SUCCESS":
+            this.game.queueMessage(
+              `${
+                cardLookup[properties.prop].name
+              } was successfully mortgaged for $${
+                cardLookup[properties.prop].mortgageValue
+              }.`
+            );
+            break;
+
+          case "NOT-OWNER":
+            this.game.queueMessage(
+              `You are not the owner of ${cardLookup[properties.prop].name}!`
+            );
+            break;
+
+          case "ALREADY-MORTGAGED":
+            this.game.queueMessage(
+              `${cardLookup[properties.prop].name} is already mortgaged.`
+            );
+            break;
+        }
         break;
       }
 
